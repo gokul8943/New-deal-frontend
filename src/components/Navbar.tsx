9744f8b293de287a9f287e0f2d249da7e6f5329a
Gokul M R <gokulmr331@gmail.com>
2024-10-27 21:48:12 +0530

    removed the search from navabar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,32 +32,9 @@ const Navbar = () => {
                         </Link>
                     </li>
                 </ul>
-                {/* <form className='relative flex items-center'>
-                    <input
-                        className='bg-slate-200 p-2 pl-4 pr-10 rounded-full focus:outline-none w-32 sm:w-56'
-                        type='text'
-                        placeholder='Search'
-                    />
-                    <button
-                        type='submit'
-                        className='absolute right-2 bg-transparent text-slate-800 focus:outline-none'
-                    >
-                        <svg
-                            xmlns='http://www.w3.org/2000/svg'
-                            fill='none'
-                            viewBox='0 0 24 24'
-                            strokeWidth={2}
-                            stroke='currentColor'
-                            className='w-5 h-5'
-                        >
-                            <path
-                                strokeLinecap='round'
-                                strokeLinejoin='round'
-                                d='M21 21l-4.35-4.35m1.56-5.44a7.5 7.5 0 11-15 0 7.5 7.5 0 0115 0z'
-                            />
-                        </svg>
-                    </button>
-                </form> */}
+                <ul>
+                    
+                </ul>
                 <ul className='ml-6'>
                     <li>
                         <Link to='/user/login'>
