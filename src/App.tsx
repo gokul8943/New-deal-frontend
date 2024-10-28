578c74385026deda351a48b669ec14fa3f7845e8
Gokul M R <gokulmr331@gmail.com>
2024-10-28 12:43:13 +0530

    product adding page added

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import ProductDeatils from "./pages/ProductDeatils"
 import ProfiePage from "./pages/ProfiePage"
+import ListingAddPage from "./pages/ListingAddPage"
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Route path='/user/login' element={<SignIn/>} />
         <Route path='/user/register' element={<SignUp/>} />
         <Route path='/profile' element={<ProfiePage/>} />
+        <Route path='/AddProduct' element={<ListingAddPage/>} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/ListingAddPage.tsx
@@ -0,0 +1,15 @@
+
+const ListingAddPage = () => {
+    return (
+        <main className="grid grid-cols-12 m-[20px] gap-2">
+            <div className="col-span-4 border h-[300px]">
+                <h1 className="text-lg font-semibold "></h1>
+            </div>
+            <div className="col-span-8 border ">
+
+            </div>
+        </main>
+    )
+}
+
+export default ListingAddPage
