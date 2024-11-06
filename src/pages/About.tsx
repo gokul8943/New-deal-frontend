dd0d85679c96438aed9cf7a32858041d1c4f1364
Gokul M R <gokulmr331@gmail.com>
2024-11-06 12:04:47 +0530

    about page design changes

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,11 @@
 
 const About = () => {
   return (
-    <div className='grid grid-col-12'>
-      <div className='py-20 px-4 max-w-6xl mx-auto'>
-        <h1 className='text-3xl font-bold mb-4 text-slate-800'>  About    <span className='text-slate-600'>FIND</span>
+    <div className='grid grid-cols-12'>
+      <div className='col-span-12 py-20 px-10 max-w-6xl mx-auto border m-[20px] bg-gradient-to-r from-green-200 to-fuchsia-200 rounded-lg shadow-lg'>
+        <h1 className="text-xl font-bold mb-4 text-green-500">Who Are We</h1>
+        <h1 className="text-2xl font-bold mb-4 p-[20px] text-gray-500">We Help To Find Your Dream</h1>
+        <h1 className='text-3xl font-bold mb-4 text-center text-violet-600'>  About    <span className='text-slate-600'>FIND</span>
           <span className='text-slate-900'>dEAL</span></h1>
         <p className='mb-4 text-slate-700'>FINDdEAL is a leading real estate agency that specializes in helping clients buy, sell, and rent properties in the most desirable neighborhoods. Our team of experienced agents is dedicated to providing exceptional service and making the buying and selling process as smooth as possible.</p>
         <p className='mb-4 text-slate-700'>
