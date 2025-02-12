2049cbdd121d6992e9461bb5938709a9f4a933ed
Gokul M R <gokulmr331@gmail.com>
2025-02-12 12:28:09 +0530

    store is added

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -1,10 +1,75 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { create } from 'zustand';
 
-export const store = configureStore({
-  reducer: {},
-})
+interface AuthState {
+  isAuthenticated: boolean;
+  accessToken?: string | null;
+  refreshToken?: string | null;
+  user: any;
+}
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+const initialAuthState: AuthState = {
+  isAuthenticated: localStorage.getItem('accessToken') ? true : false,
+  accessToken: localStorage.getItem('accessToken'),
+  refreshToken: localStorage.getItem('refreshToken'),
+  user: JSON.parse(localStorage.getItem('user') || '{}'),
+};
+
+interface AuthStore {
+  authState: AuthState;
+  login: (accessToken: string, user: any, refreshToken: string) => void;
+  logout: () => void;
+  updateUser: (user: any) => void;
+  updateAccessToken: (accessToken: string) => void;
+}
+
+const useAuthStore = create<AuthStore>((set) => ({
+  authState: { ...initialAuthState },
+  login: (accessToken: string, user: any, refreshToken: string) => {
+    localStorage.setItem('accessToken', accessToken);
+    localStorage.setItem('refreshToken', refreshToken);
+    localStorage.setItem('user', JSON.stringify(user));
+    set((state) => ({
+      authState: {
+        ...state.authState,
+        isAuthenticated: true,
+        accessToken,
+        refreshToken,
+        user,
+      },
+    }));
+  },
+  updateUser: (user) => {
+    localStorage.setItem('user', JSON.stringify(user));
+    set((state) => ({
+      authState: {
+        ...state.authState,
+        user,
+      },
+    }));
+  },
+  logout: () => {
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('refreshToken');
+    localStorage.removeItem('user');
+    set({
+      authState: {
+        ...initialAuthState,
+        isAuthenticated: false,
+        accessToken: null,
+        refreshToken: null,
+        user: null,
+      },
+    });
+  },
+  updateAccessToken: (accessToken: string) => {
+    localStorage.setItem('accessToken', accessToken);
+    set((state) => ({
+      authState: {
+        ...state.authState,
+        accessToken,
+      },
+    }));
+  },
+}));
+
+export default useAuthStore;
