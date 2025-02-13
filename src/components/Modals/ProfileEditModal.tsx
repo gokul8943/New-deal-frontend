84d60a7306527aa8310abdee0b7b3ae29e93b933
Gokul M R <gokulmr331@gmail.com>
2025-02-13 14:24:49 +0530

    edit profile modal changes

--- src/components/Modals/ProfileEditModal.tsx
+++ src/components/Modals/ProfileEditModal.tsx
@@ -1,5 +1,6 @@
 import { Form, Input, Modal } from "antd";
 import { useState } from "react";
+import useAuthStore from "../../store/authStore";
 
 interface ProfileEditModalProps {
     isModalOpen: boolean;
@@ -8,7 +9,11 @@ interface ProfileEditModalProps {
 }
 
 const ProfileEditModal: React.FC<ProfileEditModalProps> = ({ isModalOpen, handleCancel, handleOk }) => {
-    const [image, setImage] = useState<string | undefined>(''); // to preview uploaded image
+    const { authState } = useAuthStore();
+
+    const [image, setImage] = useState<string | undefined>('');
+    const [form] = Form.useForm(); 
+
 
     const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const file = event.target.files?.[0];
@@ -17,6 +22,8 @@ const ProfileEditModal: React.FC<ProfileEditModalProps> = ({ isModalOpen, handle
             setImage(imageUrl);
         }
     };
+
+    const user = authState.user
 
     return (
         <Modal title="Edit Profile" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
@@ -40,14 +47,24 @@ const ProfileEditModal: React.FC<ProfileEditModalProps> = ({ isModalOpen, handle
                         </div>
                     </div>
                     <div className="w-[85%]">
-                        <Form layout="vertical">
+                        <Form
+                            form={form}
+                            layout="vertical"
+                            initialValues={{
+                                name: user?.name || "",
+                                email: user?.email || "",
+                                phone: user?.phone || ""
+                            }}
+                            onFinish={handleOk} // Handle form submission
+                        >
                             <Form.Item label="Name" name="name" rules={[{ required: true, message: "Please enter your name" }]}>
                                 <Input placeholder="Enter your name" />
                             </Form.Item>
                             <Form.Item label="Email" name="email">
                                 <Input
                                     disabled
-                                    className="cursor-not-allowed bg-gray-100" />
+                                    className="cursor-not-allowed bg-gray-100"
+                                />
                             </Form.Item>
                             <Form.Item label="Phone" name="phone" rules={[{ required: true, message: "Please enter your phone number" }]}>
                                 <Input placeholder="Enter your phone number" />
